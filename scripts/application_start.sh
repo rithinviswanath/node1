a7a6b77e77bf973600986bbf834aad4db98a3c3a
rithinviswanath <85340308+rithinviswanath@users.noreply.github.com>
2021-08-30 17:35:27 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/node-app
+# give permission to the files inside /secure_docs directory
 
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/node-app
+sudo chmod -R 777 /home/ubuntu/node_app
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# navigate into current working directory
 
-#install node modules
+cd /home/ubuntu/node_app
+
+# install node modules
+
 npm install
 
-#start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null &
+# start our node app in the background using pm2
+
+sudo pm2 start ‘npm start.’
