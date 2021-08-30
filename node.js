81e8fc26e80ef066a125a2d38b8ce60a16c778af
rithinviswanath <85340308+rithinviswanath@users.noreply.github.com>
2021-08-30 18:31:56 +0530

    Update node.js

--- node.js
+++ node.js
@@ -3,6 +3,6 @@ var app = express();
 app.listen(5000, () => {
   console.log("Server running on port 5000");
   app.get("/url", (req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "france"]);
+    res.json(["Tony", "Lisa", "Michael", "Ginger", "dell"]);
   });
 });
